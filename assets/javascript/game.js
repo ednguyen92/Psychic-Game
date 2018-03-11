1428cbd4b5cf6705857c3f082d7bfb8a9a703bf2
Edward Nguyen <ednguyen92@gmail.com>
2018-03-10 19:40:56 -0800

    Accidently saved wrong .js file from hangman game and overwrote Psychic game .js retrieved the original through github and changed it back

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
-// Define words that computer can pick from
-var computerChoices = ["Pikachu", "Squirtle", "Charmander", "Bulbasaur"];
-
+// Define alphabet letters that computer can pick from
+var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 // Set the initial global variables
 var wins = 0;
 var losses = 0;
@@ -11,34 +10,39 @@ var guessesMade = [];
       // Have computer pick a letter and store it in letterToBeGuessed
       var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
       console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesMade + " Computer picked: " + computerGuess);
-
-          // This function is run whenever the user presses a key.
+    // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
-   
-        // Determines which key was pressed.
-        var userGuess = event.key;
 
-        var hiddenAnswer = [];
-        for (var i = 0; i < computerGuess.length; i++) {
-        hiddenAnswer[i] = "_";
-        }
-
-        
-
-       
-
-
-        guessesMade[guessesMade.length]=userGuess;
-
-    	// Displaying progress to HTML
-        var html = "<p><h1>HangMan</h1></p>" + 
-        "<p><h4>Guess what letter I\'m thinking of</h4></p>" + 
-        "<p><h4>Wins: " + wins + "</h4></p>" + 
-        "<p><h4>Losses: " + losses + "</h4></p>" + 
-        hiddenAnswer +
-        "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + 
-        "<p><h4>Your guesses so far: " + guessesMade + "</h4></p>";
-        // place html into the game ID
-        document.querySelector("#game").innerHTML = html;
+              if (userGuess === computerGuess) {
+                    wins++;
+                    console.log("You won!");
+                    guessesLeft = 9;
+            		guessesSoFar = [];
+            		computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesMade + " Computer picked: " + computerGuess);
+            	} else  {
+            		guessesMade[guessesMade.length]=userGuess;
+            		guessesLeft--;
+                } 
+                if (guessesLeft === 0){
+                    losses++;
+                    console.log("You lost!");
+                    guessesLeft = 9;
+            		guessesSoFar = [];
+            		computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesMade + " Computer picked: " + computerGuess);
+                }
+            
+            
+            
+            	// Displaying progress to HTML
+                var html = "<p><h1>The Psychic Game</h1></p>" + 
+                "<p><h4>Guess what letter I\'m thinking of</h4></p>" + 
+                "<p><h4>Wins: " + wins + "</h4></p>" + 
+                "<p><h4>Losses: " + losses + "</h4></p>" + 
+                "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + 
+                "<p><h4>Your guesses so far: " + guessesMade + "</h4></p>";
+            	// place html into the game ID
+               document.querySelector("#game").innerHTML = html;
 
         }
